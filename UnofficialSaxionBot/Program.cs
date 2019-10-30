cc414845dcf22f82d59fbdb1dfacfbf16aaef36f
123tris <xonepiece4everx@gmail.com>
2019-10-30 22:23:04 +0100

    Initial push

---
+++ UnofficialSaxionBot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+class Program
+{
+    //Call start asynchronously
+    static void Main() => new Program().StartAsync().GetAwaiter().GetResult();
+
+    private DiscordSocketClient client;
+    private CommandHandler commandHandler;
+
+    public async Task StartAsync()
+    {
+        //Create client
+        client = new DiscordSocketClient();
+
+        //Add log method to callback
+        client.Log += message =>
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        };
+
+        //Setup command handler
+        commandHandler = new CommandHandler(client);
+        await commandHandler.Init();
+
+        //Connect to server
+        Console.WriteLine("Inser your token here:\n");
+        string token = Console.ReadLine();
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+}
---
+++ UnofficialSaxionBot/TestModule.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+public class TestModule : ModuleBase<SocketCommandContext>
+{
+    [Command("ping")]
+    [Summary("It will pong, you have been warned")]
+    public Task PongAsync() => ReplyAsync("pong!");
+}
